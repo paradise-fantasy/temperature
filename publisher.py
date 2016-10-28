7be4747f4bae0ff8a2b41a2d706e3282ceb7148b
Jon-Anders Kabbe <kjabbe@gmail.com>
2016-10-28 21:22:11 +0200

    read both outside and inside temperature

--- publisher.py
+++ publisher.py
@@ -4,15 +4,16 @@ import paho.mqtt.publish as publish
 import subprocess, json
 from time import sleep
 
-def getTemp():
-    proc = subprocess.Popen('/home/pi/1-wire_temp.sh', stdout=subprocess.PIPE, shell=True)
+def getTemp(script):
+    proc = subprocess.Popen('/home/pi/temperature' + script, stdout=subprocess.PIPE, shell=True)
     (temp, error) = proc.communicate()
-    print temp
     return temp
 
-val = float(getTemp())/1000
-
-temperature = {'sensor': '1-wire', 'temperature': val}
-
-publish.single("paradise/api/temperature", json.dumps(temperature), port=8883, tls={'ca_certs':"/home/pi/ca.crt",'tls_version':2}, hostname="nyx.bjornhaug.net")
-sleep(2.5)
+def sendTemp():
+    inside = float(getTemp('inside.sh'))/1000
+    insideTemp = {'sensor': '1-wire', 'temperature': inside}
+    publish.single("paradise/api/temperature", json.dumps(insideTemp), port=8883, tls={'ca_certs':"/home/pi/ca.crt",'tls_version':2}, hostname="nyx.bjornhaug.net")
+    outside = float(getTemp('outside.sh'))/1000
+    outsideTemp = {'sensor': 'outside', 'temperature': outside}
+    publish.single("paradise/api/temperature", json.dumps(outsideTemp), port=8883, tls={'ca_certs':"/home/pi/ca.crt",'tls_version':2}, hostname="nyx.bjornhaug.net")
+    sleep(2.5)
