12fb18106ffdd01e702caf33b39d5d9f265bb5db
kjabbe <kjabbe@gmail.com>
2016-11-06 21:16:17 +0000

    Added check to disable temperature notification spam

--- publisher.py
+++ publisher.py
@@ -4,12 +4,15 @@ import paho.mqtt.publish as publish
 import subprocess, json
 from time import sleep
 
+avoid_spam = 0
+
 def getTemp(script):
     proc = subprocess.Popen("/home/pi/temperature/" + script, stdout=subprocess.PIPE, shell=True)
     (temp, error) = proc.communicate()
     return temp
 
 def sendTemp():
+    global avoid_spam
     inside = float(getTemp('inside.sh'))/1000
     print(inside)
     insideTemp = {'sensor': 'Inside', 'temperature': inside}
@@ -17,7 +20,15 @@ def sendTemp():
     outsideTemp = {'sensor': 'Outside', 'temperature': outside}
     publish.single("paradise/api/temperature", json.dumps(insideTemp), port=8883, tls={'ca_certs':"/home/pi/ca.crt",'tls_version':2}, hostname="nyx.bjornhaug.net")
     publish.single("paradise/api/temperature", json.dumps(outsideTemp), port=8883, tls={'ca_certs':"/home/pi/ca.crt",'tls_version':2}, hostname="nyx.bjornhaug.net")
-    sleep(2.5)
+    if (inside > 24):
+        if (avoid_spam<=0):
+            avoid_spam = 5
+            publish.single("paradise/notify/tts", "it is gettin hot in here, so take of all your clothes!", port=8883, tls={'ca_certs':"/home/pi/ca.crt",'tls_version':2}, hostname="nyx.bjornhaug.net")
+        else:
+            avoid_spam-=1
+    else:
+        avoid_spam=0
+    sleep(5)
 
 #print (getTemp('inside.sh'))
 while True:
