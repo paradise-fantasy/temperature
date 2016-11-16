8daa4357a429ad15a1ae3065e17071432b6ac154
Haavard Fagervoll <fagervoll@gmail.com>
2016-11-16 13:46:04 +0100

    Makes the publisher context aware. Figures out who is in Paradise and selects a randomly chosen person.

--- publisher.py
+++ publisher.py
@@ -3,8 +3,15 @@
 import paho.mqtt.publish as publish
 import subprocess, json
 from time import sleep
+import random
 
 avoid_spam = 0
+url_get_present = "http://api.komstek.no/api/presence?limit=1&sort=-_arrivedAt"
+url_get_members = "http://api.komstek.no/api/users"
+
+members = json.loads(urllib2.urlopen(url_get_members).read())
+paradiseMembers = map(lambda x: x["name"], filter(lambda x: x["room"] == "paradise", members))
+
 
 def getTemp(script):
     proc = subprocess.Popen("/home/pi/temperature/" + script, stdout=subprocess.PIPE, shell=True)
@@ -23,12 +30,29 @@ def sendTemp():
     if (inside > 24):
         if (avoid_spam<=0):
             avoid_spam = 5
-            publish.single("paradise/notify/tts", "it is gettin hot in here, so take of all your clothes!", port=8883, tls={'ca_certs':"/home/pi/ca.crt",'tls_version':2}, hostname="nyx.bjornhaug.net")
+            someone = getSomeonePresent()
+            if someone:
+                response =  "It is getting hot in here, " + someone + " please take off all your clothes!"
+                publish.single("paradise/notify/tts", response, port=8883, tls={'ca_certs':"/home/pi/ca.crt",'tls_version':2}, hostname="nyx.bjornhaug.net")
+
         else:
             avoid_spam-=1
     else:
         avoid_spam=0
     sleep(5)
+
+def getSomeonePresent():
+    peoplePresent = urllib2.urlopen(url_get_present).read()
+    peoplePresent = json.loads(peoplePresent)[0]["_value"]["status"]
+    paradisePresent = []
+    for person in peoplePresent:
+        name = person.keys()[0]
+        if name in paradiseMembers and person[name]:
+            paradisePresent.append(name)
+    if len(paradisePresent) == 0:
+        return 0
+    return random.choice(paradisePresent)
+
 
 #print (getTemp('inside.sh'))
 while True:
